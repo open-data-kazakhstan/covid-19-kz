fed87b34ade80ed584710500123de1204255278f
anuveyatsu <anuar.ustayev@gmail.com>
2020-03-26 21:13:06 +0600

    [data][s]: process deaths data based on new design of the source website.
    Also, manually fix previous data for deaths.

--- scripts/process.js
+++ scripts/process.js
@@ -12,8 +12,9 @@ fetch('https://www.coronavirus2020.kz/')
     res.text().then(page => {
       const $ = cheerio.load(page)
       const confirmed = parseInt($('span.number_cov').text())
-      const recovered = $('span', 'div.recov_bl').text().split(':')[1]
-      const deaths = 0 // TODO: handle deaths once we have confirmed death cases
+      const recovered = parseInt($('span', 'div.recov_bl').text().split(':')[1])
+      const deathsContainers = $('div:contains("Летальных случаев:")')
+      const deaths = parseInt(deathsContainers[deathsContainers.length - 1].children[1].children[0].data)
       const now = (new Date()).toISOString() // GMT+0
       fs.appendFileSync('../data/kz.csv', [now, confirmed, recovered, deaths].join(',') + '\n')
     })
